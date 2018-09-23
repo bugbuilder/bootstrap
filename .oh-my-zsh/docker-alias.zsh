6047d3adba82bf97f11a02d2a7b82af0422b9803
Nelson <nelson@bennu.cl>
2018-09-22 23:36:57 -0300

    adding alias

---
+++ .oh-my-zsh/docker-alias.zsh
@@ -0,0 +1,45 @@
+
+http(){
+    docker run -t --rm \
+        --net=host \
+        --log-driver none \
+        jess/httpie "$@"
+}
+
+terraform(){
+    docker run -it --rm \
+        -v "${HOME}:${HOME}:ro" \
+		-v "$(pwd):/usr/src/repo" \
+		-v /tmp:/tmp \
+		--workdir /usr/src/repo \
+		--log-driver none \
+        --user "${UID}:${GID}" \
+		hashicorp/terraform:0.11.8 "$@"
+}
+
+aws(){
+	docker run -it --rm \
+        -v "${HOME}/.aws:/root/.aws" \
+		--log-driver none \
+		--name aws \
+		jess/awscli "$@"
+}
+
+az(){
+	docker run -it --rm \
+		-v "${HOME}/.azure:/root/.azure" \
+		--log-driver none \
+		jess/azure-cli "$@"
+}
+
+
+kustomize(){
+    docker run -it --rm \
+        -v "${HOME}:${HOME}:ro" \
+		-v "$(pwd):/usr/src/repo" \
+		-v /tmp:/tmp \
+		--workdir /usr/src/repo \
+		--log-driver none \
+        --user "${UID}:${GID}" \
+		bennu/kustomize:v1.0.8 "$@"
+}
