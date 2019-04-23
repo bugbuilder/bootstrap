083f87d0a4bd8d66f251de9c3a90064e2b253c95
Nelson Villagrán <nelson@bennu.cl>
2019-04-22 22:32:06 -0400

    chore: adding vault

--- .oh-my-zsh/docker-alias.zsh
+++ .oh-my-zsh/docker-alias.zsh
@@ -6,17 +6,6 @@ http(){
         jess/httpie "$@"
 }
 
-terraform(){
-    docker run -it --rm \
-        -v "${HOME}:${HOME}:ro" \
-		-v "$(pwd):/usr/src/repo" \
-		-v /tmp:/tmp \
-		--workdir /usr/src/repo \
-		--log-driver none \
-        --user "${UID}:${GID}" \
-		hashicorp/terraform:0.11.8 "$@"
-}
-
 aws(){
 	docker run -it --rm \
         -v "${HOME}/.aws:/root/.aws" \
@@ -32,27 +21,23 @@ az(){
 		jess/azure-cli "$@"
 }
 
+vault() {
+    local tag="1.1.1"
+    
+    if [ -z "$1" ]; then
+        docker run --rm --name vault_client -it --cap-add IPC_LOCK vault:$tag --help
+        return 0
+    fi
+    
+    docker inspect vault_server &>/dev/null
+    
+    if [ $? -eq 1 ]; then
+        docker rm -f vault_server 2>/dev/null
+        docker run --rm --name vault_server -dit --cap-add IPC_LOCK -e 'VAULT_DEV_ROOT_TOKEN_ID=bennu' vault:$tag
+    fi
 
-kustomize(){
-    docker run -it --rm \
-        -v "${HOME}:${HOME}:ro" \
-		-v "$(pwd):/usr/src/repo" \
-		-v /tmp:/tmp \
-		--workdir /usr/src/repo \
-		--log-driver none \
-        --user "${UID}:${GID}" \
-		bennu/kustomize:v1.0.8 "$@"
+    local token="$(docker logs vault_server | grep "Root Token:" | awk '{print $3}' | sed 's/\x1b\[[0-9;]*[mGKF]//g' | sed 's/\r//g' )"
+    local addr=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' vault_server)
+
+    docker run --rm --name vault_client -it --cap-add IPC_LOCK -e "VAULT_TOKEN=${token}" -eVAULT_ADDR=http://$addr:8200 vault:$tag "$@"
 }
-
-hugo(){
-    docker run -it --rm \
-        -v "${HOME}:${HOME}:ro" \
-		-v "$(pwd):/usr/src/repo" \
-		-v /tmp:/tmp \
-        -p 1313:1313 \
-		--workdir /usr/src/repo \
-		--log-driver none \
-        --user "${UID}:${GID}" \
-		bennu/hugo:0.49 "$@"
-}
-
