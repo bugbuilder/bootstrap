e45f7ac5f3936960dc0e19fdb0bf76a5dccfefed
Nelson Villagrán <nelson@bennu.cl>
2019-04-22 22:36:56 -0400

    Update docker-alias.zsh

--- .oh-my-zsh/docker-alias.zsh
+++ .oh-my-zsh/docker-alias.zsh
@@ -23,21 +23,22 @@ az(){
 
 vault() {
     local tag="1.1.1"
-    
+
     if [ -z "$1" ]; then
-        docker run --rm --name vault_client -it --cap-add IPC_LOCK vault:$tag --help
+        docker run --rm --name vault_client -it --cap-add IPC_LOCK vault:${tag} --help
         return 0
     fi
-    
+
     docker inspect vault_server &>/dev/null
-    
+
     if [ $? -eq 1 ]; then
         docker rm -f vault_server 2>/dev/null
-        docker run --rm --name vault_server -dit --cap-add IPC_LOCK -e 'VAULT_DEV_ROOT_TOKEN_ID=bennu' vault:$tag
-    fi
+        docker run --rm --name vault_server -dit --cap-add IPC_LOCK -e 'VAULT_DEV_ROOT_TOKEN_ID=bennu' vault:${tag}
+	fi
 
     local token="$(docker logs vault_server | grep "Root Token:" | awk '{print $3}' | sed 's/\x1b\[[0-9;]*[mGKF]//g' | sed 's/\r//g' )"
     local addr=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' vault_server)
 
-    docker run --rm --name vault_client -it --cap-add IPC_LOCK -e "VAULT_TOKEN=${token}" -eVAULT_ADDR=http://$addr:8200 vault:$tag "$@"
+    docker run --rm --name vault_client -it --cap-add IPC_LOCK -eVAULT_TOKEN=${token} -eVAULT_ADDR=http://${addr}:8200 vault:${tag} "$@"
 }
+
